71216e3c972338176a607ba239186340127c82e9
Fredy Alejandro <hysterxx@users.noreply.github.com>
2022-10-04 22:09:18 -0500

    LOGIN

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    console.log('Hello world');
+    
+    function go(){
+
+        if (document.form.password.value=='CONTRASEÑA' && document.form.login.value=='USUARIO'){ 
+                document.form.submit(); 
+            } 
+            else{ 
+                 alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+            } 
+        } 
 
 })
